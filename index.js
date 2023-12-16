19efb804bec83630d9101d7a2d9d5cb0c6a6b9cc
Ahs <92954050+AhslexR@users.noreply.github.com>
2023-12-16 19:37:08 -0400

    ss
    first push

---
+++ index.js
@@ -0,0 +1,8 @@
+// hola-mundo.js
+
+function holaMundo() {
+    console.log("¡Hola, Mundo!");
+  }
+  
+  module.exports = holaMundo; // Exporta la función para que pueda ser importada en las pruebas
+  
